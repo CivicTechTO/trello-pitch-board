d84135d372a0eaf4ef73ac6473d000f129410d3b
Patrick Connolly <patrick.c.connolly@gmail.com>
2018-05-22 15:13:34 -0400

    Testing modal from first principles.

---
+++ js/client2.js
@@ -0,0 +1,26 @@
+Vue.prototype.$http = axios
+// register modal component
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
+// start app
+new Vue({
+  el: '#app',
+  data: {
+    showModal: false,
+    mydata: [],
+  },
+  created: function () {
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      vm = this
+      vm.$http.get('data/data.json')
+      .then(response => {
+        vm.mydata = response.data
+      })
+    },
+  }
+})
